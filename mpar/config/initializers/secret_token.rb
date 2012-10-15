aae2e04e5629bf7f9b547f7ed76fc6796fa84a4a
Marta Janicka <mjanicka@p109-5.labpk.inf.ug.edu.pl>
2012-10-15 13:20:42 +0200

    lab1

---
+++ mpar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpar::Application.config.secret_token = '29ccfcb513f281de67d542cd38015fc4b22545d308e579a6953284085ec39dfa7923045ad3f5cd422fbc8faf6f0790d4fee2a2e2a5b943c9bf9f6124c88ffaea'
