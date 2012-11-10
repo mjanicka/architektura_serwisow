01a47a49a3318ce2582c27cf3ff0d4b6ed776d75
mjanicka <m.janickaa@gmail.com>
2012-11-10 15:28:16 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 architektura_serwisow
 =====================
+system zarządzania danymi pacjęta przez lekarza 
+lekarz ma wielu pacjętów
+pacjęt ma wiele wizyt
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pro::Application.config.secret_token = '526eefcc7e6639bed1e8294704537c6e6a5ab7d1dc295a100c98b38ec49cd2033b17a52f7cd028580017916ffca3da415cfcaebc2b7ee0d7ec3c05d1a050c2b8'
